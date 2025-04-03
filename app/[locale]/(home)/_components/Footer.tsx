94afb694f19e7fc5a4fc158145b84e81e3848f8c
JiuRanYa <1084350607@qq.com>
2025-04-03 18:21:07 +0800

    feat: add tooltip component and integrate it into Footer for enhanced user interaction

--- app/[locale]/(home)/_components/Footer.tsx
+++ app/[locale]/(home)/_components/Footer.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import { useTranslations } from 'next-intl'
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/core/components/tooltip'
 
 export default function Footer() {
  const t = useTranslations('Footer')
@@ -27,6 +28,19 @@ export default function Footer() {
          <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
         </svg>
        </a>
+       <Tooltip>
+        <TooltipTrigger asChild>
+         <a href="mailto:siyuantong7@gmail.com" className="text-gray-500 hover:text-gray-800">
+          <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
+           <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
+           <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
+          </svg>
+         </a>
+        </TooltipTrigger>
+        <TooltipContent>
+         siyuantong7@gmail.com
+        </TooltipContent>
+       </Tooltip>
       </div>
      </div>
 
